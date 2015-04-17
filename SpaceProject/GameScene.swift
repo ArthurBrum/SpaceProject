3431e38efab7301c897a789417d5223a5e9d882c
Arthur Brum <Arthur@MacBook-Pro-de-Arthur.local>
2015-04-17 05:41:49 -0300

    First relevant change

--- SpaceProject/GameScene.swift
+++ SpaceProject/GameScene.swift
@@ -9,14 +9,47 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var Player = SKSpriteNode(imageNamed: "PlayerGalaga.png")
+
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
+        Player.position = CGPointMake(self.size.width/2, self.size.height/6)
+        var Timer = NSTimer.scheduledTimerWithTimeInterval(0.2, target: self, selector: Selector("SpawnBullets"), userInfo: nil, repeats: true)
+        
+        var EnemyTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("SpawEnemies"), userInfo: nil, repeats: true)
+        
+        self.addChild(Player)
+    }
+    
+    
+    func SpawnBullets(){
+        
+        var Bullet = SKSpriteNode(imageNamed: "BulletGalaga.png")
+        Bullet.zPosition = -5
+        Bullet.position = CGPointMake(Player.position.x, Player.position.y)
+        
+        
+        let action = SKAction.moveToY(self.size.height + 30, duration: 1)
+        Bullet.runAction(SKAction.repeatActionForever(action))
+        
+        
+        self.addChild(Bullet)
+    }
+    
+    func SpawnEnemies(){
+        var Enemy = SKSpriteNode(imageNamed: "EnemyGalaga.png")
+        var MinValue = self.size.width/8
+        var MaxValue = self.size.width - 20
+        var SpawnPoint = UInt32(MaxValue - MinValue)
+        Enemy.position = CGPoint(x: CGFloat(arc4random_uniform(SpawnPoint)), y: self.size.height)
+        
+        let action = SKAction.moveToY(-70, duration: 3.0)
+        Enemy.runAction(SKAction.repeatActionForever(action))
+        
+        
+        self.addChild(Enemy)
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
@@ -25,17 +58,21 @@ class GameScene: SKScene {
         for touch in (touches as! Set<UITouch>) {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            Player.position.x = location.x
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            // Be careful, enables teletransporting super power xPPP
+            //Player.position.y = location.y
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+        }
+    }
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        for touch in (touches as! Set<UITouch>) {
+            let location = touch.locationInNode(self)
             
-            sprite.runAction(SKAction.repeatActionForever(action))
+            Player.position.x = location.x
+
             
-            self.addChild(sprite)
         }
     }
    
