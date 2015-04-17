723c9eab8c657a5cfebdf746b5720a11118827b9
Arthur Brum <Arthur@MacBook-Pro-de-Arthur.local>
2015-04-17 07:33:33 -0300

    Collision working

--- SpaceProject/GameScene.swift
+++ SpaceProject/GameScene.swift
@@ -8,21 +8,80 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+
+struct PhysicsCatagory{
+    static let Enemy : UInt32 = 1   //00000000000000000000000000000001
+    static let Bullet : UInt32 = 2  //00000000000000000000000000000010
+    static let Player : UInt32 = 3  //00000000000000000000000000000011
+}
+
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    var Score = Int()
     var Player = SKSpriteNode(imageNamed: "PlayerGalaga.png")
+    var ScoreLbl = UILabel()
 
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
-        Player.position = CGPointMake(self.size.width/2, self.size.height/6)
-        var Timer = NSTimer.scheduledTimerWithTimeInterval(0.2, target: self, selector: Selector("SpawnBullets"), userInfo: nil, repeats: true)
+        physicsWorld.contactDelegate = self
         
-        var EnemyTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("SpawEnemies"), userInfo: nil, repeats: true)
+        
+        Player.position = CGPointMake(self.size.width/2, self.size.height/6)
+        
+        Player.physicsBody = SKPhysicsBody(rectangleOfSize: Player.size)
+        Player.physicsBody?.affectedByGravity = false
+        Player.physicsBody?.dynamic = false
+        Player.physicsBody?.categoryBitMask = PhysicsCatagory.Player
+        Player.physicsBody?.contactTestBitMask = PhysicsCatagory.Enemy
+        
+        var Timer = NSTimer.scheduledTimerWithTimeInterval(0.25, target: self, selector: Selector("SpawnBullets"), userInfo: nil, repeats: true)
+        
+        var EnemyTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("SpawnEnemies"), userInfo: nil, repeats: true)
         
         self.addChild(Player)
+        
+        ScoreLbl.text = "\(Score)"
+        ScoreLbl = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 20))
+        ScoreLbl.backgroundColor = UIColor(red: 0.1, green: 3, blue: 0.1, alpha: 0.3)
+        ScoreLbl.textColor = UIColor.whiteColor()
+        
+        self.view?.addSubview(ScoreLbl)
     }
     
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        var firstBody : SKPhysicsBody = contact.bodyA
+        var secondBody : SKPhysicsBody = contact.bodyB
+        
+        if ((firstBody.categoryBitMask == PhysicsCatagory.Enemy) && (secondBody.categoryBitMask == PhysicsCatagory.Bullet)) ||
+            ((firstBody.categoryBitMask == PhysicsCatagory.Bullet) && (secondBody.categoryBitMask == PhysicsCatagory.Enemy)){
+            
+                CollisionWithBullet(firstBody.node as! SKSpriteNode, Bullet: secondBody.node as! SKSpriteNode)
+    
+                
+        }else if ((firstBody.categoryBitMask == PhysicsCatagory.Enemy) && (secondBody.categoryBitMask == PhysicsCatagory.Player)) ||
+            ((firstBody.categoryBitMask == PhysicsCatagory.Player) && (secondBody.categoryBitMask == PhysicsCatagory.Enemy)){
+                
+                CollisionWithPerson(firstBody.node as! SKSpriteNode, Person: secondBody.node as! SKSpriteNode)
+        }
+        
+    }
+    
+    func CollisionWithBullet (Enemy: SKSpriteNode, Bullet: SKSpriteNode){
+        Enemy.removeFromParent()
+        Bullet.removeFromParent()
+        
+        Score++
+        
+        ScoreLbl.text = "\(Score)"
+    }
+    
+    func CollisionWithPerson(Enemy: SKSpriteNode, Person: SKSpriteNode){
+        Enemy.removeFromParent()
+        Person.removeFromParent()
+    }
     
     func SpawnBullets(){
         
@@ -31,23 +90,38 @@ class GameScene: SKScene {
         Bullet.position = CGPointMake(Player.position.x, Player.position.y)
         
         
-        let action = SKAction.moveToY(self.size.height + 30, duration: 1)
-        Bullet.runAction(SKAction.repeatActionForever(action))
+        Bullet.physicsBody = SKPhysicsBody(rectangleOfSize: Bullet.size)
+        Bullet.physicsBody?.affectedByGravity = false
+        Bullet.physicsBody?.dynamic = false
+        Bullet.physicsBody?.categoryBitMask = PhysicsCatagory.Bullet
+        Bullet.physicsBody?.contactTestBitMask = PhysicsCatagory.Enemy
         
+        
+        let action = SKAction.moveToY(self.size.height + 30, duration: 1)
+        let actionDone = SKAction.removeFromParent()
+        Bullet.runAction(SKAction.sequence([action, actionDone]))
         
         self.addChild(Bullet)
     }
     
     func SpawnEnemies(){
         var Enemy = SKSpriteNode(imageNamed: "EnemyGalaga.png")
+        Enemy.zPosition = -5
         var MinValue = self.size.width/8
         var MaxValue = self.size.width - 20
         var SpawnPoint = UInt32(MaxValue - MinValue)
         Enemy.position = CGPoint(x: CGFloat(arc4random_uniform(SpawnPoint)), y: self.size.height)
         
-        let action = SKAction.moveToY(-70, duration: 3.0)
-        Enemy.runAction(SKAction.repeatActionForever(action))
+        Enemy.physicsBody = SKPhysicsBody(rectangleOfSize: Enemy.size)
+        Enemy.physicsBody?.affectedByGravity = false
+        Enemy.physicsBody?.dynamic = true
+        Enemy.physicsBody?.categoryBitMask = PhysicsCatagory.Enemy
+        Enemy.physicsBody?.contactTestBitMask = PhysicsCatagory.Bullet
         
+        
+        let action = SKAction.moveToY(-70, duration: 3.0)
+        let actionDone = SKAction.removeFromParent()
+        Enemy.runAction(SKAction.sequence([action, actionDone]))
         
         self.addChild(Enemy)
     }
